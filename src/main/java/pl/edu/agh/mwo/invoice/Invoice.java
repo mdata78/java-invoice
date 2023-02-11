6c291dde6c51b423451a424eec11a2a68f7cc2f0
Gosia Data <data@agh.edu.pl>
2023-02-11 12:14:44 +0100

    last

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,22 +2,33 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+//    private Collection<Product> products;
+
+    private Map<Product, Integer> products = new HashMap<>();
 
     public void addProduct(Product product) {
         // TODO: implement
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+//        for (int i = 0; i <quantity; i++){
+//            products.add(product);
+//        }
+        this.products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
            return BigDecimal.ZERO;
+    }
+
+    public BigDecimal getNetPrice() {
+        return BigDecimal.ZERO;
     }
 
     public BigDecimal getTax() {
